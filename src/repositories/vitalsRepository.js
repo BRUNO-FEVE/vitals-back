6b38a9cf9ef75ac08c0e9de413d2ca2311cd59a4
GabrielBianconiconi <gabrielbianconiconi@gmail.com>
2025-05-10 10:10:09 -0300

    fix: vitalsModel name and Repo

--- src/repositories/vitalsRepository.js
+++ src/repositories/vitalsRepository.js
@@ -1,54 +1,29 @@
-const BaseEntity = require('./baseEntity');
+const BaseRepository = require('./baseRepository');
+const VitalsModel = require('../models/vitalsModel');
+const VitalsEntity = require('../entities/vitalsEntity');
 
-class VitalsEntity extends BaseEntity {
-  constructor(data = {}) {
-    super(data);
-    this.patientId = data.patientId || null;
-    this.temperature = data.temperature || null;
-    this.heartRate = data.heartRate || null;
-    this.oxygenSaturation = data.oxygenSaturation || null;
-    this.systolicPressure = data.systolicPressure || null;
-    this.diastolicPressure = data.diastolicPressure || null;
-    this.weight = data.weight || null;
+class VitalsRepository extends BaseRepository {
+  constructor() {
+    super(VitalsModel);
   }
 
-  validate() {
-    if (!this.patientId) {
-      throw new Error('Patient ID is required');
-    }
-    if (this.temperature === null) {
-      throw new Error('Temperature is required');
-    }
-    if (this.heartRate === null) {
-      throw new Error('Heart rate is required');
-    }
-    if (this.oxygenSaturation === null) {
-      throw new Error('Oxygen saturation is required');
-    }
-    if (this.systolicPressure === null) {
-      throw new Error('Systolic pressure is required');
-    }
-    if (this.diastolicPressure === null) {
-      throw new Error('Diastolic pressure is required');
-    }
-    if (this.weight === null) {
-      throw new Error('Weight is required');
-    }
-    return true;
+  async createVitals(vitalsEntity) {
+    vitalsEntity.validate();
+    const vitalsData = vitalsEntity.toObject();
+    delete vitalsData.id;
+    
+    const vitals = await this.create(vitalsData);
+    return new VitalsEntity(vitals);
   }
 
-  toObject() {
-    return {
-      ...super.toObject(),
-      patientId: this.patientId,
-      temperature: this.temperature,
-      heartRate: this.heartRate,
-      oxygenSaturation: this.oxygenSaturation,
-      systolicPressure: this.systolicPressure,
-      diastolicPressure: this.diastolicPressure,
-      weight: this.weight
-    };
+  async updateVitals(id, vitalsEntity) {
+    vitalsEntity.validate();
+    const vitalsData = vitalsEntity.toObject();
+    delete vitalsData.id;
+    
+    const vitals = await this.updateById(id, vitalsData);
+    return vitals ? new VitalsEntity(vitals) : null;
   }
 }
 
-module.exports = VitalsEntity;
+module.exports = VitalsRepository;
